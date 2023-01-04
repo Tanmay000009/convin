66d764a1766e7e7a582cf8b30bbcc26db9f1204e
tanmay000009 <tanmayvyas009@gmail.com>
2023-01-05 01:57:35 +0530

    style landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Users } from "./features/user/Users";
 
 export const App = () => {
   return (
-    <div className="App">
+    <div className="App bg-[#232946] min-h-screen min-w-screen text-white">
       <h1
         style={{
           display: "flex",
           justifyContent: "center",
         }}
+        className="p-4 text-6xl text-[#b8c1ec]"
       >
         Convin task
       </h1>
